bbbbfcf228e0017b67f10a494fb359fd7578ef6b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2026-01-13 11:47:44 +0600

    brands js :bulb:

--- src/Components/Home/Brands/Brands.jsx
+++ src/Components/Home/Brands/Brands.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
 import Brand from './Brand';
-
+// brands
 const Brands = () => {
     const { dark } = useContext(AuthContext)
     const [data,setdata] = useState([])
